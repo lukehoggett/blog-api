b24ce49589b5dd2ceee24f1b1f2af7e6eab84b5c
Luke Hoggett <luke.hoggett@gmail.com>
2015-08-03 06:18:11 +0000

    Strongloop skeleton

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('HW\n');
-}).listen(8080, '172.31.12.214');
-console.log('Server running at http://172.31.12.214:8080/');
